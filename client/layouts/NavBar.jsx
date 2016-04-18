7679f12d9afdd48ea202ba7a8351437d0a70c810
Charlie Roth <roth47@purdue.edu>
2016-04-18 13:37:19 -0400

    Added nav bar, bootstrap styling

---
+++ client/layouts/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import AccountsUI from '../AccountsUI.jsx';
+
+export default class NavBar extends React.Component {
+  render() {
+
+    var accountsStyle = {
+      paddingTop: 15
+    }
+
+    return(
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">impronto</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/events">Events</a></li>
+                <li><a href="/">How It Works</a></li>
+                <li><a href="/support">Support</a></li>
+                <li style={accountsStyle}><AccountsUI /></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
