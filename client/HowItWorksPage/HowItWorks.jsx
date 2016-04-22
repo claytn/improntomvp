4dac7d34a8fea7b025684efa4e122b5baa8c1ad9
marsha88 <marsha88@purdue.edu>
2016-04-22 18:52:54 -0400

    claytonPush

--- client/HowItWorksPage/HowItWorks.jsx
+++ client/HowItWorksPage/HowItWorks.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class HowItWorks extends React.Component {
   render() {
     return(
-      <div className="row">
+      <div className="row" id="howitworks">
         <h1 className="text-center">How It Works</h1>
       </div>
     );
