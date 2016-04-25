d82ca18494ff78831f30dce9be1a6f9dc72c9799
marsha88 <marsha88@purdue.edu>
2016-04-25 19:45:44 -0400

    updated nav

--- client/layouts/NavBar.jsx
+++ client/layouts/NavBar.jsx
@@ -5,28 +5,37 @@ export default class NavBar extends React.Component {
   render() {
     return(
       <div>
-        <nav className="navbar navbar-light bg-faded navbar-fixed-top">
-          <ul className="nav navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/events">Events</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/howitworks">How It Works</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/support">Support</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/addevent">Add Event</a>
-            </li>
-            <li className="nav-item">
-              <AccountsUI />
-            </li>
-          </ul>
-        </nav>
+      
+
+  <nav class="navbar navbar-light bg-faded">
+  <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+    &#9776;
+  </button>
+  <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+    <a class="navbar-brand" href="#">Impronto</a>
+    <ul class="nav navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/events">Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/howitworks">How It Works</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/support">Support</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/addevent">Add Event</a>
+      </li>
+      <li className="nav-item">
+        <AccountsUI />
+      </li>
+
+    </ul>
+  </div>
+</nav>
       </div>
     );
   }
