c688d327cede8682f96e684be00dd6b14f196a79
Charlie Roth <roth47@purdue.edu>
2016-05-06 19:25:33 -0400

    Added material styling

--- client/layouts/NavBar.jsx
+++ client/layouts/NavBar.jsx
@@ -6,28 +6,15 @@ export default class NavBar extends React.Component {
   render() {
     return(
       <div>
-        <nav className="navbar navbar-light navbar-fixed-top">
-          <a className="navbar-brand ubuntu white" id="logoHeader" href="/"><span className="yellow">Im</span>pronto</a>
-          <button className="navbar-toggler hidden-sm-up pull-right" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
-            &#9776;
-          </button>
-          <div className="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
-            <ul className="nav navbar-nav" id="navList">
-              <li className="nav-item listItem">
-                <a className="nav-link white ubuntu" href="/events"><span className="boost">Events</span></a>
-              </li>
-              <li className="nav-item listItem">
-                <a className="nav-link white ubuntu" href="#howItWorks"><span className="boost">How It Works</span></a>
-              </li>
-              <li className="nav-item listItem">
-                <a className="nav-link white ubuntu" href="/support"><span className="boost">Support</span></a>
-              </li>
-              <li className="nav-item listItem">
-                <a className="nav-link white ubuntu" href="/addevent"><span className="boost">Add Event</span></a>
-              </li>
-              <li className="nav-item">
-                <button className="btn  btn-sm btn-primary-outline" onClick={()=> this.refs.simpleDialog.show()}>Log In</button>
-              </li>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="/" className="brand-logo">Logo</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="/events">events</a></li>
+              <li><a href="#howItWorks">how it works</a></li>
+              <li><a href="/support">support</a></li>
+              <li><a href="/addevent">add event</a></li>
+              <li><button className="waves-effect waves-light btn" onClick={()=> this.refs.simpleDialog.show()}>Log In</button></li>
             </ul>
           </div>
         </nav>
